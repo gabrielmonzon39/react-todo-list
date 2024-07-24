72e15ab4b5bd0993ebde7f10a00bc639dc00b7c4
Gabriel Monzon <gabriel.monzon39@gmail.com>
2024-07-24 00:52:44 -0600

    ready as single component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,92 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!newItem) return;
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        },
+      ];
+    });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed,
+          };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input id="item" type="text" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            id="item"
+            type="text"
+          />
         </div>
-        <button className="btn" type="submit">Add Item</button>
+        <button className="btn" type="submit">
+          Add Item
+        </button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li className="todo-item">
-          <label>
-            <input type="checkbox" />
-            <span>Item 1</span>
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.length === 0 && <li className="todo-item">No items</li>}
+        {todos.map((todo) => {
+          return (
+            <li className="todo-item" key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => {
+                    toggleTodo(todo.id, e.target.checked);
+                  }}
+                />
+                <span>{todo.title}</span>
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
-
     </>
-  )
+  );
 }
