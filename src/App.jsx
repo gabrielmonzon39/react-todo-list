1e9b969196bb540c938b1d59376287d6b1d3153d
Gabriel Monzon <gabriel.monzon39@gmail.com>
2024-07-24 00:30:22 -0600

    static version ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import "./styles.css"
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input id="item" type="text" />
+        </div>
+        <button className="btn" type="submit">Add Item</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li className="todo-item">
+          <label>
+            <input type="checkbox" />
+            <span>Item 1</span>
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+
+    </>
   )
 }
